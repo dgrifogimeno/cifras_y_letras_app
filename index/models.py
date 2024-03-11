195f5d94196b66ce5d0433ac0fac56cfe07ff384
dgrifogimeno <david.grifo.gimeno@gmail.com>
2024-03-11 20:23:59 +0100

    My first Model in Django + Admin site

--- index/models.py
+++ index/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Game(models.Model):
+    game_name = models.CharField(max_length = 20)
+    description = models.CharField(max_length = 50)
+    times_played = models.IntegerField(default = 0)
+
+    def __str__(self) -> str:
+        return self.game_name + "-> " + self.description + " (" + str(self.times_played) + " veces jugado)"
+    
+    def is_popular(self) -> bool:
+        return self.times_played > 10
+    
